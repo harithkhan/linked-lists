ef2256afef6b7059025b2ee420880b204da20224
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-12 14:25:32 +0800

    feat: add rudimentary LinkedList and Node classes Add rudimentary LinkedList and Node classes in new module linked-lists.js.

---
+++ linked-lists.js
@@ -0,0 +1,15 @@
+class LinkedList {
+
+    constructor() {
+        this.nodes = [];
+    }
+
+    
+}
+
+class Nodes {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}

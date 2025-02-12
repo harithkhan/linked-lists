1722117b46a17ac6994c601e88fa79a818466b14
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-12 15:18:27 +0800

    feat: add temporary solution to append function Add temporary solution to append function that stores new nodes in an array. Project should not be done this way, so this was just experimantal to help with visualisation.

--- linked-lists.js
+++ linked-lists.js
@@ -1,15 +1,34 @@
-class LinkedList {
-
-    constructor() {
-        this.nodes = [];
-    }
-
-    
-}
-
-class Nodes {
+class Node {
     constructor(value = null, nextNode = null) {
         this.value = value;
         this.nextNode = nextNode;
     }
 }
+
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            this.nodes = [];
+            this.nodes.push(newNode);
+        } else {
+            this.nodes[this.nodes.length - 1].nextNode = newNode;
+            this.nodes.push(newNode);
+        }
+    }
+}
+
+
+
+const testLinkedList = new LinkedList;
+testLinkedList.append("cat");
+testLinkedList.append("dog");
+testLinkedList.append("chicken");
+console.log(testLinkedList);
+console.log(testLinkedList.nodes[2])
